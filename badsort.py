9b7aa31264fc2bd0e086105dc5130f8e7bb7de37
alex <cgazelka@gmail.com>
2025-12-25 21:53:01 +0500

    new thing

--- badsort.py
+++ badsort.py
@@ -1,13 +1,6 @@
 class Solution:
-    def FindTheGuest(self, liste: list, target: str):
-        for i in range(len(liste)):
-            if liste[i] == target:
-                # find first var of the list and then do until target
-                first = liste.index(0)
-                stored = []
-                while True:
-                    if first != target:
-                        stored.append(first)
-                        first += 1
-                    else:
-                        return stored
+    def Tofix(self, list: list, target: int):
+        for index in range(len(list)):
+            if list[index] > 100:
+                return index
+        return -1
