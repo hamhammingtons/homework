694dc9fbffc7853f288d256b2368f072838d9482
alex <cgazelka@gmail.com>
2026-01-07 20:34:24 +0500

    l;

--- general/mattch.py
+++ general/mattch.py
@@ -1,10 +1,5 @@
-def tosort(dic: dict):
-    match dic:
-        case {"type": "message", "sender": sender, "content": content}:
-            return f"message from {sender}: {content}"
-        case {"type": "login", "user": user}:
-            return f"User logged in {user}"
-        case {"type": "err", "code": Code}:
-            return "err", Code
-        case _:
-            return "idk figure it out sir"
+item = {"category": "fruit", "details": {"name": "apple", "price": 1.50}}
+
+match item:
+    case {"category": "fruit", "name": "apple", "price": sd}:
+        print("the price is" + str(sd))
