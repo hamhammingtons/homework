206c73dd7fe2bd91c822be39f43daa51810a604f
alex <cgazelka@gmail.com>
2026-01-07 20:37:36 +0500

    fixed fat

--- general/mattch.py
+++ general/mattch.py
@@ -1,5 +1,5 @@
 item = {"category": "fruit", "details": {"name": "apple", "price": 1.50}}
 
 match item:
-    case {"category": "fruit", "name": "apple", "price": sd}:
-        print("the price is" + str(sd))
+    case {"category": "fruit", "details": {"name": "apple", "price": sd}}:  # i forgor
+        print("the price is:\t" + str(sd))
