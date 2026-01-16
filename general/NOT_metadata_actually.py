92d14831437a0e29bc4ce2ecab369647ec45e7d0
alex <cgazelka@gmail.com>
2026-01-16 15:04:04 +0500

    general/metadata_with_functions_console_refactored.py : i renamed the file to metadata because it now contains the right purpose of metadatas.

--- general/NOT_metadata_actually.py
+++ general/NOT_metadata_actually.py
@@ -6,82 +6,59 @@ tosum.uid = 12  # type: ignore
 print(tosum.uid)  # type: ignore
 print(tosum.__dict__)  # uid is now in a secret dict
 
+
 # techincally mentioning i could refactor my code which is the console_project.py to have metadata instead of dict{key: [tuple values]}
 # we will make a console for a person to echo something first. lets make a class i think
 
 
 class Console:
     def __init__(self) -> None:
-        self.commands = {"echo": {"func": self.echo_func, "uid": 12, "prm_req": 0}}
+        self.comms = {}
 
-    def echo_func(self, arg_list, uid):
-        if not arg_list:
-            print("err fatal, arg list not provided.")
-        else:
-            print(f"uid: {uid}, returned: {" ".join(arg_list)}")
+    def toRegister(self, name, uid, params_allowed):
+        def wrapper(
+            func,
+        ):  # remember dont pass func in the outer body only in the wrapper thank you
+            func.uid = uid
+            func.name = name
+            func.params_allowed = params_allowed
+
+            self.comms[name] = func
+            return func
+
+        return wrapper
 
     def to_run(self):
         while True:
-            user_str = input("> ").lower().split()
-            if not user_str:
+            user_input = input(f"> ").lower().split()
+            if not user_input:
                 continue
 
-            name = user_str[0]
-
-            if name in [
-                "exit" or "quit"
-            ]:  # used to be if 'name == "exit" or "quit" ' -> quit will be always true unless we do the machinacions again, because str = true
+            name, args = user_input[0], user_input[1:]
+            if name in ["exit", "quit"]:
                 break
 
-            args = user_str[1:]
+            if name in self.comms:
+                func = self.comms[name]
 
-            if name in self.commands:
-                found_func = self.commands[name]
-                func = found_func["func"]
-                uid = found_func["uid"]
-                prm_req = found_func["prm_req"]
-
-                if len(args) < prm_req:
-                    print(
-                        f"err: parameters must be at least {prm_req} in {func.__name__}"
-                    )
+                if len(args) < func.params_allowed:
+                    print(f"Err: {name} needs {func.params_allowed} args")
                 else:
-                    func(args, uid)
+                    func(args)
             else:
-                print(f"err: unknown command, expression: \n{user_str}")
+                print("Unknown command")
 
 
-clasic = Console()
-clasic.to_run()
+new = Console()
 
 
-# ^^^ this isnt really metadata, this is just "Data Placement".
-# if wed like to assign metadata we would actually do metadata
+@new.toRegister(
+    name="echo", uid=3, params_allowed=0
+)  # refactored thing is pretty weird but cool and much more readable.
+def echo(listothings: list):
+    print(" ".join(listothings))
 
-"""
-console bla bla bla:
-    def __init__(self):
-        self.some_func.uid = 12
-        self.some_func.prm_allowed = 1
-        
-        then we atually map it out 
-        
-        commands = {
-        "echo": some_func (already has __dict__ inside it and has uid and prm allowed, you can check.)
-        }
-          --btw means hidden dict inside func (metadata -like a label)
-            also dont forget a function is an object thats why we can assign dicts to it like that...
 
-        def to_run(self):
-            while True:
-                ...
-                func = commands[name] 
-                uid = func.uid
-                prm_req = func.prm_req -- again because it has __dict__
-                ...
-        """
+new.to_run()
 
-# TODO: I forgot to say but actually we can use a decorator here to not do the manual thing where we assign values!:
-# self.some_func.uid = 12
-# self.some_func.prm_allowed = 1
-# ... --- SO ill need to learn how to do decorators based of this!
+# TODO: i no longer need this todo, i did what i wanted to do.
