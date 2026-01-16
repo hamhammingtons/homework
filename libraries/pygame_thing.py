02cea87aa984584738a003577e33def59462ac13
alex <cgazelka@gmail.com>
2026-01-16 17:00:35 +0500

    dw

--- libraries/pygame_thing.py
+++ libraries/pygame_thing.py
@@ -1,14 +1,27 @@
-import pygame, sys
+import pygame
+import sys
+import time
 
 pygame.init()
+screen = pygame.display.set_mode((640, 480))
+pygame.display.set_caption("Mouse Motion Demo")
 
-screen_size = pygame.display.set_mode((800, 600), pygame.RESIZABLE)
-pygame.display.set_caption("Hi")
+object_size = 20
+object_color = (255, 0, 0)
+object_rect = pygame.Rect(0, 0, object_size, object_size)
 
-print(pygame.display.get_active())
-
-while True:
+running = True
+while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
+            running = False
+
+        elif event.type == pygame.MOUSEMOTION:
+            object_rect.center = event.pos
+
+    screen.fill((255, 255, 255))
+    pygame.draw.rect(screen, object_color, object_rect)
+    pygame.display.flip()
+
+pygame.quit()
+sys.exit()
