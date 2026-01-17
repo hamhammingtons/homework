d58f3c27ba02b85693f439aa34ddd941766cab74
alex <cgazelka@gmail.com>
2026-01-17 23:52:53 +0500

    d

---
+++ libraries/os/os_first_grasp.py
@@ -0,0 +1,7 @@
+import os
+
+url = "https://www.youtube.com/watch?v=tJxcKyFMTGo"  # click run to open
+
+os.system(f'start chrome "{url}"')
+
+# TODO: come back to this and watch "https://www.youtube.com/watch?v=tJxcKyFMTGo&t=71s" or start the program to watch
