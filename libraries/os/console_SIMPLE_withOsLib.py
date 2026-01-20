138b02aca18d1dabd17f818026b1a6f188a1c84c
alex <cgazelka@gmail.com>
2026-01-20 23:17:55 +0500

    deleted some ai slop but added suggested things such as shlex which was suggested by ai. also refactored some things so they work better

--- libraries/os/console_SIMPLE_withOsLib.py
+++ libraries/os/console_SIMPLE_withOsLib.py
@@ -1,4 +1,5 @@
 import os
+import shlex  # 1 use case, which is ai written too. i dont know this module.
 
 
 class Console:
@@ -16,10 +17,7 @@ class Console:
         return wrapper
 
     def specifically_for_os_module(self, func):
-        def wrapper(self, user_args):
-            # self  -> Console instance
-            # user_args -> list of strings from the user
-
+        def wrapper(self, user_args):  # user_args == params READ BELOW PLZ
             if not user_args:
                 print("err: path missing")
                 return
@@ -38,11 +36,15 @@ class Console:
 
     def to_run(self):
         while True:
-            user_input = input("> ").lower().split()
+            user_input = shlex.split(  # BIG FIX
+                input("> ")
+            )  # this is where this comes in handy, idk how to use it though. it was suggested by ai. so its just
+            # for so when we do ls "OneDrive/Рабочий стол" it gets "Рабочий стол" instead of "Рабочий ".
             if not user_input:
                 continue
 
-            name, params = user_input[0], user_input[1:]
+            name = user_input[0]
+            params = user_input[1:]
 
             if name in ["exit", "quit"]:
                 break
@@ -53,13 +55,15 @@ class Console:
                 if len(params) < func.params_allowed:
                     print(f"Err: {name} needs {func.params_allowed} args")
                 else:
-                    # Pass self AND params
                     func(self, params)
             else:
                 print("Unknown command")
 
 
 new = Console()
+
+os.chdir(os.environ["USERPROFILE"])
+# not really that important but we can use it if we want to go to home directly
 
 
 @new.toRegister(name="echo", uid=3, params_allowed=0)
@@ -75,5 +79,32 @@ def listdir(self, listothings: list):
 
 new.to_run()
 
-# TODO: remove this ai slop and make the function yourslef
-# i hate how it did the os module wrapper
+""" this was heinous to do but i guess it works 
+main things to note:
+please for the love of god never use indirect indexing it is really hard to do and debug
+^ it isnt evil exactly but its recomended to not use this 
+i mean this crap:
+
+name = user_input[0]
+params = user_input[1:]
+
+Use something like making these parameters global by adding them to the object itself
+self.name = user_input[0] 
+...
+
+^ btw not recomended, just dont mix up this part:
+func(self, params)
+parameters = what the user typed, idk why i used user_input it made it way confusing
+
+OR also you can do 
+name, *params = input("> ").split() to not use indirect indexing
+"""
+# --- exceptions ---
+"""""
+FOR MAKING EXCEPTIONS (such as what i used for the os module)
+
+Please use (self, function) first and then whatever you want
+because if were doing not the manual way of doing something, we will need to first execute the exceptions things 
+and then run the uid or store them in like a data base or whatever. I mean attaching metadata. 
+
+"""
