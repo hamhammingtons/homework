893c9131385b323cc3988eb9bb4595411a37f06a
alex <cgazelka@gmail.com>
2026-02-01 19:17:50 +0500

    j

--- libraries/powershel_lol/PS_Intermediate/computer_architecture_stuff/basic_things.ps1
+++ libraries/powershel_lol/PS_Intermediate/computer_architecture_stuff/basic_things.ps1
@@ -1,6 +1,3 @@
-Get-Process | Select-Object Name, Id, VirtualMemorySize64, WorkingSet64 | Sort-Object WorkingSet64 -Descending | Select-Object -First 10
-Get-Process | Select-Object Name, PageFaults | Sort-Object PageFaults -Descending | Select-Object -First 5
-
-$mem = Get-Process | Measure-Object -Property VirtualMemorySize64, WorkingSet64 -Sum
-Write-Host "The Illusion (Virtual): $([math]::round($mem.SumVirtualMemorySize64 / 1GB, 2)) GB"
-Write-Host "The Reality (Physical): $([math]::round($mem.SumWorkingSet64 / 1GB, 2)) GB"
+# This shows how many "Committed" pages (Valid Bit likely 1) 
+# versus "Reserved" pages (Valid Bit is 0) a process has.
+Get-Process chrome | Select-Object Name, @{Name = "Committed_MB"; Expression = { $_.PagedMemorySize64 / 1MB } }, @{Name = "WorkingSet_MB"; Expression = { $_.WorkingSet64 / 1MB } }
