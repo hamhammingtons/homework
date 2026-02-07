2b5d0ad6b5c76a01e85ad36291b57acd137a8cca
ham hammingtons <cgazelka@gmail.com>
2026-02-07 22:16:32 +0500

    Implement matrix operations for row sum and diagonal product
    Added matrix operations to calculate sum of row 2 and product of diagonal elements.

--- homework_for_it_step/newerr.py
+++ homework_for_it_step/newerr.py
@@ -7,3 +7,15 @@ max_idx = np.argmax(arr)
 print(arr)
 print(max_val)
 print(max_idx)
+# 2 zhadanie
+matrix = np.array([
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+])
+
+row2_sum = matrix[1, 0] + matrix[1, 1]
+product_diag = np.prod(np.diag(matrix))
+
+print(row2_sum)
+print(product_diag)
